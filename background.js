c5ffb412bba598ffebd3c3e51b7771cb026f50ce
Wills Bithery <wills@willsbithrey.com>
2013-11-30 14:51:45 +0000

    Trying get content script injected

--- background.js
+++ background.js
@@ -2,12 +2,16 @@
 
 chrome.runtime.onInstalled.addListener(function() {
 	console.log("Extension installed.");
-	debugger
-	chrome.webNavigation.onCommitted.addListener(function(e) {
+
+	chrome.webNavigation.onCompleted.addListener(function(e) {
 		console.log("URL Filter matched.", e);
+		chrome.tabs.executeScript(null, {file: "content_script.js"}, function () {
+			console.log('content_script.js was injected into the active tab.');
+		});
 	}, {
 		url: [
-			{pathSuffix: '.js'}
+			{urlContains: ''}
 		]
 	});
+
 });
