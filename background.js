7d5b67d0e872c16c8b9688456dc0f96dc0c6fc7c
Wills Bithery <wills@willsbithrey.com>
2013-11-30 15:26:28 +0000

    Tidy up background.js a little

--- background.js
+++ background.js
@@ -1,17 +1,45 @@
-// Copyright (c) 2013 WillS Bithrey
+/* global chrome */
 
-chrome.runtime.onInstalled.addListener(function() {
-	console.log("Extension installed.");
+// Copyright (c) 2013 Wills Bithrey
+(function () {
+	'use strict';
 
-	chrome.webNavigation.onCompleted.addListener(function(e) {
-		console.log("URL Filter matched.", e);
-		chrome.tabs.executeScript(null, {file: "content_script.js"}, function () {
+	/**
+	 * Callback which runs immediately after the extension is installed.
+	 * @return {undefined}
+	 */
+	function onInstalled () {
+		console.log('Extension installed.');
+		registerEventListeners();
+	};
+
+	/**
+	 * Sets up the extension so that if a supported type of file is detected the
+	 * extensions content script is called. Uses Chrome's UrlFilters toi only
+	 * run the content script when a supported file type is detected.
+	 * 
+	 * @return {undefined}
+	 */
+	function registerEventListeners () {
+		chrome.webNavigation.onCompleted.addListener(onNavigationCompleted, {
+			url: [
+				{ pathSuffix: '.js' }
+			]
+		});
+	};
+
+	/**
+	 * Dynamically injects the extensions content script.
+	 * @return {undefined}
+	 */
+	function onNavigationCompleted (e) {
+		console.log('URL Filter matched.', e);
+		chrome.tabs.executeScript(null, {file: 'content_script.js'}, function () {
 			console.log('content_script.js was injected into the active tab.');
 		});
-	}, {
-		url: [
-			{urlContains: ''}
-		]
-	});
+	};
 
-});
+	// Bind to Chrome's `onInstalled` event to perform extension initialisation
+	// once the extension is installed.
+	chrome.runtime.onInstalled.addListener(onInstalled);
+}());
